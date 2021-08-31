11c311f67d940a12c438310121b79d8f6cf24f97
Brian Henning <henninb@msn.com>
2021-08-31 06:16:03 -0500

    wip

---
+++ src/main/groovy/finance/services/DatabaseMigrationService.groovy
@@ -0,0 +1,20 @@
+package finance.services
+
+import org.flywaydb.core.Flyway
+import ratpack.service.Service
+import ratpack.service.StartEvent
+
+import javax.sql.DataSource
+
+
+class DatabaseMigrationService implements Service {
+    void onStart(StartEvent e) {
+//        DataSource dataSource = e.registry.get(DataSource)
+//
+//        Flyway flyway = new Flyway()
+//
+//        flyway.setDataSource(dataSource)
+//
+//        flyway.migrate()
+    }
+}
